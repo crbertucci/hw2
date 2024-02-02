27ff8c5721a18a8e0b55f5c721032e9a2150df2d
Chris Bertucci <christopher.bertucci@kellogg.northwestern.edu>
2024-02-02 04:08:37 +0000

    212208

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_02_034700) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_02_040554) do
+  create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "year_released"
+    t.string "rated"
+    t.integer "studio_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "studios", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
